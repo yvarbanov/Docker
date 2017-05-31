267a3686c4cec6321784015a3c8ba35695dab626
yvarbanov <yordanasir@gmail.com>
2017-05-31 17:08:35 +0200

    Add files

--- Flask_Supervisord/app2.py
+++ Flask_Supervisord/app2.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello World'
+    return "Hello World, I'am app 2"
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0')
+    app.run(debug=True, host='0.0.0.0', port=5002)
